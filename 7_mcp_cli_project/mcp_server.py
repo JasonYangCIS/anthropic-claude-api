b3dc35c8ec9aaf345822339499ebd2bf6b04ec1d
Jason Yang <jasonYangCIS@gmail.com>
2026-04-07 15:21:58 -0700

    feat: add two tools for reading and replacing string in doc

--- 7_mcp_cli_project/mcp_server.py
+++ 7_mcp_cli_project/mcp_server.py
@@ -1,3 +1,4 @@
+from pydantic import Field
 from mcp.server.fastmcp import FastMCP
 
 mcp = FastMCP("DocumentMCP", log_level="ERROR")
@@ -12,8 +13,37 @@ docs = {
     "spec.txt": "These specifications define the technical requirements for the equipment.",
 }
 
-# TODO: Write a tool to read a doc
-# TODO: Write a tool to edit a doc
+# 1: Tool to read a doc
+@mcp.tool(
+    name="read_doc_contents",
+    description="Reads the contents of a document and return it as a string",
+)
+
+def read_document(
+    doc_id: str = Field(description="Id of the document to read")
+):
+    if doc_id not in docs:
+        raise ValueError(f"Doc with id {doc_id} not found")
+    
+    return docs[doc_id]
+
+# 2. Tool to edit a doc with string replace
+@mcp.tool(
+    name="edit_doc_contents",
+    description="Edit a document by replacing a string in the documents content with a new string."
+)
+
+def edit_document(
+    doc_id: str = Field(description="Id of the document to edit"),
+    old_string: str = Field(description="The text to replace. Must match exactly, including whitespace."),
+    new_string: str = Field(description="The new text to replace the old text with.")
+):
+    if doc_id not in docs:
+        raise ValueError(f"Doc with id {doc_id} not found")
+    
+    docs[doc_id] = docs[doc_id].replace(old_string, new_string)
+    return docs[doc_id]
+
 # TODO: Write a resource to return all doc id's
 # TODO: Write a resource to return the contents of a particular doc
 # TODO: Write a prompt to rewrite a doc in markdown format
